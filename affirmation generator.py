6e5469a5fe6a2fa8df7beb1a718f6662a33d26f4
Karuru Midhun Sai <164469674+midhunsai04@users.noreply.github.com>
2024-08-11 17:29:39 +0530

    Add files via upload

---
+++ affirmation generator.py
@@ -0,0 +1,34 @@
+print("===AFFIRMATION GENERATOR===")
+name=input("what is your name? :")
+if(name=="Midhun" or name=="sai"):
+  print("""
+  hello there!
+  It is good to see you again sir.
+  we have been discussing about how good the nature is and you have appeared from no where.
+  come join us.
+  """)
+else:
+  print(name,"it doesnt match with our data")
+hair=input("how much hair do you have?: ")
+if(hair==0,1,2,3,4,5,6,7,8,9,10):
+  print("aww pity on you!")
+  color=input("what is your hair color? :")
+  if color=="white" or color=="grey":
+    print("ohh lord have mercy!")
+  else:
+    print("that is ok")
+print("one should always maintain their hair good.",name,"i think you have to concentrate on your hair more than you do now.")
+listen=input("are you listenening to my words??")
+if(listen == "Yes"):
+  print("good")
+  talking=input("what are we talking about?: ")
+  if(talking=="about hair"):
+    print("nice")
+  else:
+    print("bye")
+elif(listen=="omg yesss!"):
+  print("great")
+else:
+  print("alright dont show me your face")
+
+
